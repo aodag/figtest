43802c9dd75a68a83e637ddca144905810aca00e
Atsushi Odagiri <aodagx@gmail.com>
2014-05-10 09:47:14 +0900

    drone

--- app.py
+++ app.py
@@ -2,16 +2,22 @@ from flask import Flask
 from redis import Redis
 import os
 app = Flask(__name__)
-redis = Redis(
-    host=os.environ.get('REDIS_1_PORT_6379_TCP_ADDR'),
-    port=int(os.environ.get('REDIS_1_PORT_6379_TCP_PORT'))
-)
+
+redis = None
+
 
 @app.route('/')
 def hello():
     redis.incr('hits')
     return 'Hello World! I have been seen %s times.' % redis.get('hits')
 
-if __name__ == "__main__":
+def main():
+    global redis
+    redis = Redis(
+        host=os.environ.get('REDIS_1_PORT_6379_TCP_ADDR'),
+        port=int(os.environ.get('REDIS_1_PORT_6379_TCP_PORT'))
+    )
     app.run(host="0.0.0.0", debug=True)
 
+if __name__ == "__main__":
+    main()
